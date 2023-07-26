f41e2b2e63b5c861e434c27643ed9bf05439b300
青木智滉 <@MSI>
2023-07-26 14:30:12 +0900

    タイトルバーの変更

--- base/WinApp.cpp
+++ base/WinApp.cpp
@@ -90,7 +90,7 @@ void WinApp::CreateGameWindow(const wchar_t* title, int32_t kClientWidth, int32_
 	//ウィンドウの生成
 	hwnd_ = CreateWindow(
 		wc_.lpszClassName, //利用するクラス名
-		L"CG2", //タイトルバーの文字(何でも良い)
+		L"TR1", //タイトルバーの文字(何でも良い)
 		WS_OVERLAPPEDWINDOW, //よく見るウィンドウスタイル
 		CW_USEDEFAULT, //表示X座標(Windowsに任せる)
 		CW_USEDEFAULT, //表示Y座標(Windowsに任せる)
